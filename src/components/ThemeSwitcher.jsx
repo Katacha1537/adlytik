28a6e6b1237097f3506fedf0ab9e08db9fec681a
Lucas Katacha <lucasgabrieljaci19@gmail.com>
2023-12-07 20:57:49 -0300

    projeto padrão

---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Switch } from "@nextui-org/react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+export function ThemeSwitcher() {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null;
+
+    const toggleTheme = (isDarkMode) => {
+        setTheme(isDarkMode ? 'dark' : 'light');
+    };
+
+    return (
+        <Switch
+            checked={theme === 'dark'}
+            onChange={(e) => toggleTheme(e.target.checked)}
+            size="lg"
+            color="secondary"
+            startContent={<FaMoon />} // Icone para o modo escuro
+            endContent={<FaSun />} // Icone para o modo claro
+        >
+            {theme === 'dark' ? 'Modo Escuro' : 'Modo Claro'}
+        </Switch>
+    );
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@nextui-org/react'
+import { ThemeSwitcher } from '../../components/ThemeSwitcher'
+
+import { FaMoon, FaSun } from "react-icons/fa";
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
